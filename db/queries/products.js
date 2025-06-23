9e582c0156a82eb41cf9dafe002ac976a38dbff9
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-06-22 22:36:34 -0400

    updated products schema, query, router & some review stuff

--- db/queries/products.js
+++ db/queries/products.js
@@ -1,5 +1,24 @@
  import db from "../client.js"
- 
+
+ //GET all products
+  export async function getProducts (){
+    const sql = `
+    SELECT * FROM products;
+    `
+    const {rows: products} = await db.query(sql)
+    return products
+  }
+
+ //GET products by ID
+
+   export async function getProduct ({id}){
+    const sql = `
+    SELECT * FROM orders WHERE id = $1;
+    `
+    const {rows: product} = await db.query(sql, [id])
+    return product
+  }
+
  /** @returns the entry created according to the provided details */
   export async function createProduct({ title, description, price }) {
     const sql = `
@@ -10,3 +29,4 @@
     const { rows: product } = await db.query(sql, [title, description, price]);
     return product[0];
   }
+
--- db/queries/reviews.js
+++ db/queries/reviews.js
@@ -10,3 +10,25 @@
     const { rows: review } = await db.query(sql, [rating, comment, product_id]);
     return review[0];
   }
+
+//    //Get Product Reviews
+
+//    export async function getReviews ({id}){
+//     const sql = `
+//     SELECT * FROM reviews WHERE product_id = $1;
+//     `
+//     const {rows: orders} = await db.query(sql, [id])
+//     return orders
+//   }
+
+//  //Post Product Review
+
+//   export async function createReview({ rating, comment, product_id }) {
+//     const sql = `
+//       INSERT INTO reviews (rating, comment, product_id)
+//       VALUES ($1, $2, $3)
+//       RETURNING *;
+//     `;
+//     const { rows: product } = await db.query(sql, [rating, comment, product_id]);
+//     return product[0];
+//   }
