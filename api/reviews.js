ffad794e57af831c5d9a6774506d3c0d3075d408
ChanellVS <zariachanellb@gmail.com>
2025-06-23 20:04:34 -0400

    Added api and GET for reviews

---
+++ api/reviews.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const router = express.Router();
+export default router;
+
+import { getReviews } from '../db/queries/reviews.js';
+
+// GET /reviews/:product_id
+
+router.route('/reviews/:product_id').get(async (req, res) => {
+    const product_id  = req.params.product_id;
+    if (!product_id) {
+        return res.status(400).send('Product ID is required');
+    } 
+    
+    const reviews = await getReviews(product_id);
+
+    if (!reviews || reviews.length === 0) {
+        return res.status(404).send('There were no reviews found for this product');
+    }
+
+    res.status(200).send(reviews);
+})
