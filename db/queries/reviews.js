fcb33a4f7e507faeb3bac981effd989c61d19bf8
ChanellVS <zariachanellb@gmail.com>
2025-06-23 20:10:35 -0400

    Comments removed

--- db/queries/reviews.js
+++ db/queries/reviews.js
@@ -1,6 +1,7 @@
  import db from "../client.js"
  
  /** @returns the entry created according to the provided details */
+
   export async function createReview({ rating, comment, product_id }) {
     const sql = `
       INSERT INTO users (rating, comment, product_id)
@@ -13,22 +14,22 @@
 
 //    //Get Product Reviews
 
-//    export async function getReviews ({id}){
-//     const sql = `
-//     SELECT * FROM reviews WHERE product_id = $1;
-//     `
-//     const {rows: orders} = await db.query(sql, [id])
-//     return orders
-//   }
+   export async function getReviews ({id}){
+    const sql = `
+    SELECT * FROM reviews WHERE product_id = $1;
+    `
+    const {rows: orders} = await db.query(sql, [id])
+    return orders
+  }
 
-//  //Post Product Review
+ //Post Product Review
 
-//   export async function createReview({ rating, comment, product_id }) {
-//     const sql = `
-//       INSERT INTO reviews (rating, comment, product_id)
-//       VALUES ($1, $2, $3)
-//       RETURNING *;
-//     `;
-//     const { rows: product } = await db.query(sql, [rating, comment, product_id]);
-//     return product[0];
-//   }
+  export async function createReview({ rating, comment, product_id }) {
+    const sql = `
+      INSERT INTO reviews (rating, comment, product_id)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const { rows: product } = await db.query(sql, [rating, comment, product_id]);
+    return product[0];
+  }
